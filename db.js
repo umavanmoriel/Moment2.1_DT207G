7487d8dc6966da7b373f99fad7c4a41f6a671b81
umavanmoriel <umeda19971102@gmail.com>
2026-04-20 17:27:16 +0200

    Fixat db.js för Railway MySQL

--- db.js
+++ db.js
@@ -1,24 +1,12 @@
 const mysql = require('mysql2');
 
 // Skapar anslutning
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'DT207G_Moment2'
-});
+const connection = mysql.createConnection(process.env.MYSQL_URL);
 
 // Skapa promise
 const promiseConnection = connection.promise();
 
-// Ansluter till databasen DT207G_Moment2
-connection.connect((err) => {
-    if (err) {
-        console.error('Kunde inte ansluta till databasen:', err.message);
-        return;
-    }
-    console.log('Ansluten till databasen DT207G_Moment2');
-});
+
 
 // Exporterar promise anslutning
 module.exports = promiseConnection;
